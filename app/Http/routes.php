9886824bba9e9b5f260b9e424dba0d8b47356576
Kris-2015 <krishnadev.rnc@gmail.com>
2016-09-28 20:23:05 +0530

    Add comments for all routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,26 +11,36 @@
 |
 */
 
+// Default view of application
 Route::get('/', function () {
     return view('auth/login');
 });
 
+// Registration page 
 Route::get('signUp', 'AuthController@registration');
+
+// Login Page
 Route::get('login', 'AuthController@login');
+
+// Post Request action after register button is clicked
 Route::post('doregister', [
     'as' => 'doregister', 
     'uses' => 'AuthController@doRegister'
 ]);
 
+// Post Request action after login button is clicked
 Route::post('dologin', [
     'as' => 'dologin', 
     'uses' => 'AuthController@dologin'
 ]);
+
+// Logging out the user
 Route::get('logout', [
     'as' => 'logout',
     'uses' => 'AuthController@logout'
 ]);
 
+// Groupping the routes for restricted resources
 Route::group(['middleware' => 'auth'], function () {
     Route::get('home', 'TemplateController@home');
     Route::get('profile', 'TemplateController@profile');
